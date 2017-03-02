5a2eab8aa55c668c904c1dac9b18f8c627d43563
Jérémie Aubert <jeremie.aub@gmail.com>
2017-03-02 14:43:52 +0100

    Updated licence
    Added Maxime as the zimlet author.

--- com_cloudtemple_zimletplus.js
+++ com_cloudtemple_zimletplus.js
@@ -1,6 +1,6 @@
 /*
 com_cloudtemple_zimletplus
-Copyright (C) 2016-2017 - CloudTemple
+Copyright (C) 2016-2017 - Maxime Désécot -  CloudTemple
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
